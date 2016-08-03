ac2e9f3bd2d292d741d3a916d228c5623439d506
donkimber <donkimber@gmail.com>
2016-08-03 13:12:29 -0700

    fixed bug with socketIO URL

--- TeleViewer/static/js/WVCom.js
+++ TeleViewer/static/js/WVCom.js
@@ -15,8 +15,10 @@ WV.WVCom = function()
     this.types = {};
     if (WV.useSocketIO) {
 	report("Getting socket.io socket");
+	report("location.server:"+location.protocol);
 	//var sioUrl = "http://"+location.hostname+":3000";
-	var sioUrl = "http://"+location.hostname;
+	//var sioUrl = "http://"+location.hostname;
+	var sioUrl = location.protocol+"//"+location.hostname;
 	report("socketIO url "+sioUrl)
 	//WV.socket = io("http://platonia:3000");
         WV.socket = io(sioUrl);
