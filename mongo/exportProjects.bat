e8e8fb68cf44c3ce5d21ae1ab0314da81b2fc30b
donkimber <donkimber@gmail.com>
2016-09-18 15:39:02 -0700

    added a backup script

---
+++ mongo/exportProjects.bat
@@ -0,0 +1,5 @@
+set PATH=%PATH%;"C:\Program Files\MongoDB\Server\3.2\bin"
+
+mongoexport --db wv_data --collection project --out bakckup\project.json
+
+
---
+++ mongo/importProjects.sh
@@ -0,0 +1,4 @@
+
+mongoimport --db wv_data --collection project --file bakckup/project.json
+
+
