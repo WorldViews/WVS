5f72cf2186c3b900bb2ed603b5700dfcc1ad9ca5
Jim Vaughan <jimv@mrjim.com>
2017-04-26 00:03:08 -0700

    Formatting

--- react/src/components/VideoRoom/instructions.js
+++ react/src/components/VideoRoom/instructions.js
@@ -13,7 +13,12 @@ export default class Instructions extends React.Component {
           <div className="instructions">
             <h1>Welcome to the Cherry Blossom Festival at Memorial Park in Cupertino</h1>
             <hr/>
-            <p>The Cupertino Toyokawa Sister City program promotes cultural awareness and friendship between the cities of Cupertino and Toyokawa, Japan.  The relationship celebrated its 39th anniversary in 2017. The friendship is built through the annual student exchange program.  The cultural awareness is promoted through the annual Cherry Blossom Festival held every year on the last weekend in April.</p>
+            <p>The Cupertino Toyokawa Sister City program promotes cultural awareness
+               and friendship between the cities of Cupertino and Toyokawa, Japan. 
+               The relationship celebrated its 39th anniversary in 2017. The friendship
+               is built through the annual student exchange program.  The cultural awareness
+               is promoted through the annual Cherry Blossom Festival held every year on the
+               last weekend in April.</p>
 
           <a href="http://www.cupertinotoyokawa.org/">Cupertino-Toyokawa Sister City Program</a>
           <a href="http://cupertino.org/index.aspx?page=1079"City of Cupertino Cherry Blossom</a>
