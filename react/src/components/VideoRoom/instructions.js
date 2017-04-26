044dc1a884a77728b7e1af718cf708f43c17e710
Yulius Tjahjadi <yultide@yahoo.com>
2017-04-26 01:38:47 -0700

    clean up instructions

--- react/src/components/VideoRoom/instructions.js
+++ react/src/components/VideoRoom/instructions.js
@@ -11,17 +11,23 @@ export default class Instructions extends React.Component {
     return (
         <div className={this.props.className}>
           <div className="instructions">
-            <h1>Welcome to the Cherry Blossom Festival at Memorial Park in Cupertino</h1>
+            <div className="text-center">
+              <h1>Welcome to the Cherry Blossom Festival at Memorial Park in Cupertino</h1>
+            </div>
             <hr/>
-            <p>The Cupertino Toyokawa Sister City program promotes cultural awareness
-               and friendship between the cities of Cupertino and Toyokawa, Japan. 
-               The relationship celebrated its 39th anniversary in 2017. The friendship
-               is built through the annual student exchange program.  The cultural awareness
-               is promoted through the annual Cherry Blossom Festival held every year on the
-               last weekend in April.</p>
+            <div className="lead">
+              <p>The Cupertino Toyokawa Sister City program promotes cultural awareness
+                and friendship between the cities of Cupertino and Toyokawa, Japan.
+                The relationship celebrated its 39th anniversary in 2017. The friendship
+                is built through the annual student exchange program.  The cultural awareness
+                is promoted through the annual Cherry Blossom Festival held every year on the
+                last weekend in April.</p>
 
-          <a href="http://www.cupertinotoyokawa.org/">Cupertino-Toyokawa Sister City Program</a>
-          <a href="http://cupertino.org/index.aspx?page=1079"City of Cupertino Cherry Blossom</a>
+              <ul>
+                <li><a href="http://www.cupertinotoyokawa.org/">Cupertino-Toyokawa Sister City Program</a></li>
+                <li><a href="http://cupertino.org/index.aspx?page=1079">City of Cupertino Cherry Blossom</a></li>
+              </ul>
+            </div>
           </div>
         </div>
         )
